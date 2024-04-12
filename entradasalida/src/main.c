2de6644a68159e2f4f0270b6dc6159ff14a3e2a9
smalirat <smaliratcanga@frba.utn.edu.ar>
2024-04-12 23:05:33 +0000

    conexion entre e/s con kernel y memoria

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,8 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <utils/hello.h>
+#include <main.h>
 
 int main(int argc, char* argv[]) {
-    decir_hola("una Interfaz de Entrada/Salida");
+    
+    int conexion_kernel, conexion_memoria;
+	char* ip;
+	char* puerto_kernel;
+    char* puerto_memoria;
+	char *clave_kernel, *clave_memoria;
+
+	t_log* logger;
+	t_config* config;
+
+    logger = iniciar_logger();
+
+    config = iniciar_config();
+
+    //obtenemos las direcciones de puertos e ip's en config
+    ip = config_get_string_value(config,"IP");
+	puerto_kernel = config_get_string_value(config,"PUERTO_KERNEL");
+    puerto_memoria = config_get_string_value(config,"PUERTO_MEMORIA");
+	clave_kernel = config_get_string_value(config,"CLAVE_KERNEL");
+    clave_memoria = config_get_string_value(config,"CLAVE_MEMORIA");
+
+    //creo las conexiones
+
+    conexion_kernel = crear_conexion(ip, puerto_kernel);
+	conexion_memoria = crear_conexion(ip, puerto_memoria);
+
+
+	// Enviamos a los servidores
+	enviar_mensaje(clave_kernel,conexion_kernel);
+    enviar_mensaje(clave_memoria,conexion_memoria);
+
+
+	//Armamos y enviamos los paquetes
+	paquete(conexion_kernel);
+    paquete(conexion_memoria);
+
+	terminar_programa(conexion_kernel, conexion_memoria, logger, config);
+
     return 0;
 }
+
--- utils/src/utils/hello.h
+++ utils/src/utils/hello.h
@@ -1,8 +1,11 @@
 #ifndef UTILS_HELLO_H_
 #define UTILS_HELLO_H_
-
 #include <stdlib.h>
 #include <stdio.h>
+
+#define PUERTO_KERNEL "4444"
+#define PUERTO_MEMORIA "5555"
+#define PUERTO_CPU "5555"
 
 /**
 * @fn    decir_hola
