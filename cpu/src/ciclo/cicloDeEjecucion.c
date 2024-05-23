015b43c9e4774dd1a65452559bbee21e8b561507
Lucio <luciovv010@gmail.com>
2024-05-23 22:53:10 +0000

    CPUcheckpoint2 no terminado

--- cpu/src/ciclo/cicloDeEjecucion.c
+++ cpu/src/ciclo/cicloDeEjecucion.c
@@ -23,14 +23,17 @@ void cicloDeEjecucion (int *socketMemoria, int *socketDISPATCH, int *socketINTER
 void execute(char* instruccion[], Proceso *procesoCPU){
 
     char operacion = instruccion[0];
-
+    char* primerParametro = instruccion[1];
+    char* segundoParametro = instruccion[2];
+    
     switch (operacion)
     {
     case SET:
-        /* code */
+        void* registro = obtenerRegistro(primerParametro, procesoCPU);
+        registro = atoi(&segundoParametro);
         break;
     case SUM:
-        /* code */
+        
         break;
     case SUB:
         /* code */
@@ -41,7 +44,41 @@ void execute(char* instruccion[], Proceso *procesoCPU){
     case IO_GEN_SLEEP:
         /* code */
         break;
-    
+    case EXIT:
+
+        break;
+    default:
+        break;
+    }
+}
+
+void* obtenerRegistro(char* registro, Proceso *procesoCPU){
+    switch (registro)
+    {
+    case ax:
+        return procesoCPU->registros.ax;
+        break;
+    case eax:
+        return procesoCPU->registros.eax;
+        break;
+    case bx:
+        return procesoCPU->registros.bx;
+        break;
+    case ebx:
+        return procesoCPU->registros.ebx;
+        break;
+    case cx:
+        return procesoCPU->registros.cx;
+        break;
+    case ecx:
+        return procesoCPU->registros.ecx;
+        break;
+    case dx:
+        return procesoCPU->registros.dx;
+        break;
+    case edx:
+        return procesoCPU->registros.edx;
+        break;
     default:
         break;
     }
--- cpu/src/connKernel/connKernel.h
+++ cpu/src/connKernel/connKernel.h
@@ -1,6 +1,18 @@
 #ifndef KERNEL_CONN_H
 #define KERNEL_CONN_H
 
+#include <stdint.h>
+#include <utils/funcionesBuffer/funcionesBuffer.h>
+
+/**
+ * @brief struct para pasar los parametros a la funcion del hilo
+ */
+typedef struct
+{
+    int *socket;
+    Proceso *procesosCPU;
+} parametros_hilo;  
+
 /**
 * @fn    manageDISPATCH
 * @brief Función en el hilo para la conexión dispatch con el Kernel.
